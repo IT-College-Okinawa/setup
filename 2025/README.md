3cd1013b344c669bdd06948fbf52575732c57f43
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2025-04-07 16:35:37 +0900

    vagrant 追加

--- 2025/README.md
+++ 2025/README.md
@@ -7,3 +7,9 @@ bash ~/Downloads/install-libvirt.sh
 ```
 3. パスワードを聞かれた入力
 4. 一度PCを再起動
+
+5. [install-vagrant.sh](./install-vagrant.sh) をダウンロード
+6. 次のコマンドを実行
+```bash
+bash ~/Downloads/install-vagrant.sh
+```
---
+++ 2025/install-vagrant.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo <<'EOL' | sudo tee /etc/apt/sources.list.d/hashicorp.sources
+Types: deb
+URIs: https://apt.releases.hashicorp.com/
+Components: main
+Signed-By: /usr/share/keyrings/hashicorp-archive-keyring.gpg
+EOL
+
+sudo apt update && sudo apt install vagrant
+
+sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+sudo apt install nfs-kernel-server
+sudo systemctl enable --now nfs-server
+sudo apt build-dep ruby-libvirt
+sudo apt install -y ebtables dnsmasq-base
+sudo apt install -y install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev unzip
+
+vagrant plugin install vagrant-libvirt
