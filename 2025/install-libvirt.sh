1f3d0670f5f62c94dca4048ba534a53f6df1164d
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2025-04-07 16:08:19 +0900

    libvirt install scripts

---
+++ 2025/install-libvirt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+sudo systemctl enable --now libvirtd
+
+sudo adduser "$(id -un)" libvirt
+sudo adduser "$(id -un)" kvm
