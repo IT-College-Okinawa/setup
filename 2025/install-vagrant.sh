e7dc78f07e924209237b537d0ac2cb00536eaee9
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2025-04-07 16:37:57 +0900

    vagrantのインストールスクリプトを修正

--- 2025/install-vagrant.sh
+++ 2025/install-vagrant.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo echo 'install vagrant...'
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 echo <<'EOL' | sudo tee /etc/apt/sources.list.d/hashicorp.sources
@@ -9,10 +10,10 @@ Components: main
 Signed-By: /usr/share/keyrings/hashicorp-archive-keyring.gpg
 EOL
 
-sudo apt update && sudo apt install vagrant
+sudo apt update && sudo apt install -y vagrant
 
 sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
-sudo apt install nfs-kernel-server
+sudo apt install -y nfs-kernel-server
 sudo systemctl enable --now nfs-server
 sudo apt build-dep ruby-libvirt
 sudo apt install -y ebtables dnsmasq-base
