1240853aab90f0d0707b9068d012bd805b93caf1
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2025-04-15 18:09:53 +0900

    sedスクリプト修正2

--- 2025/modify-vagrant-templates.sh
+++ 2025/modify-vagrant-templates.sh
@@ -1,4 +1,4 @@
 sudo echo 'modify template Vagrantfile...'
 
-sudo sed -i -e '/^  # SHELL/a   config.ssh.forward_agent = true' /opt/vagrant/embedded/gems/gems/vagrant-2.4.3/templates/commands/init/Vagrantfile.erb
-sudo sed -i -e '/^  # SHELL/a   config.vm.synced_folder ".", "/vagrant", type: "nfs", nfs_version: "4", nfs_udp: false' /opt/vagrant/embedded/gems/gems/vagrant-2.4.3/templates/commands/init/Vagrantfile.erb
+sudo sed -i -e '/^  # SHELL/a \  config.ssh.forward_agent = true' /opt/vagrant/embedded/gems/gems/vagrant-2.4.3/templates/commands/init/Vagrantfile.erb
+sudo sed -i -e '/^  # SHELL/a \  config.vm.synced_folder ".", "/vagrant", type: "nfs", nfs_version: "4", nfs_udp: false' /opt/vagrant/embedded/gems/gems/vagrant-2.4.3/templates/commands/init/Vagrantfile.erb
