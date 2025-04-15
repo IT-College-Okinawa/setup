b57e52bc184134e28c1a568e19c46974c5a279b7
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2025-04-15 16:01:12 +0900

    なんかスクリプトがおかしかった?

--- 2025/install-vagrant.sh
+++ 2025/install-vagrant.sh
@@ -18,8 +18,8 @@ sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.
 sudo apt install -y nfs-kernel-server
 sudo systemctl enable --now nfs-server
 sudo apt install -y build-essential
-sudo apt build-dep -y ruby-libvirt
 sudo apt install -y ebtables dnsmasq-base libguestfs-tools
 sudo apt install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-fog-libvirt unzip
+sudo apt build-dep -y ruby-libvirt
 
 vagrant plugin install vagrant-libvirt
